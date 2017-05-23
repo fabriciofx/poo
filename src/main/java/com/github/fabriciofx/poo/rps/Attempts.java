1ca5064cebd59afa3c99afc1eac7e40a721fb639
Fabricio Cabral <fabriciofx@gmail.com>
2017-05-23 14:51:10 -0300

    Renaming

--- src/main/java/com/github/fabriciofx/poo/rps/Attempts.java
+++ src/main/java/com/github/fabriciofx/poo/rps/Attempts.java
@@ -1,18 +1,18 @@
 package com.github.fabriciofx.poo.rps;
 
 public final class Attempts {
-	private final Match round;
+	private final Match match;
 	private final int max;
 	
-	public Attempts(final Match round, final int max) {
-		this.round = round;
+	public Attempts(final Match match, final int max) {
+		this.match = match;
 		this.max = max;
 	}
 	
 	public boolean matches() {
 		int t = 0;
 		while (t++ < this.max) {
-			this.round.start();
+			this.match.start();
 		}
 		return t <= this.max;
 	}

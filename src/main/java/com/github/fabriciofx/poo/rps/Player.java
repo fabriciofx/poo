87fedac4adee4b97ebed917d619140cc36a6daae
Fabricio Cabral <fabriciofx@gmail.com>
2017-05-24 00:00:54 -0300

    the code has been moved to here: https://github.com/fabriciofx/rockpaperscissors

--- src/main/java/com/github/fabriciofx/poo/rps/Player.java
+++
@@ -1,7 +0,0 @@
-package com.github.fabriciofx.poo.rps;
-
-public interface Player {
-	String name();
-	
-	Move move(Ui ui);
-}
--- src/main/java/com/github/fabriciofx/poo/rps/Ui.java
+++
@@ -1,7 +0,0 @@
-package com.github.fabriciofx.poo.rps;
-
-public interface Ui {
-	void print(String message);
-	
-	char character(String message, String pattern);
-}
